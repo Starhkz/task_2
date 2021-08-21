44825dd3ea636ae501872f206693fb73acb567a4
Starhkz <67955282+Starhkz@users.noreply.github.com>
2021-08-21 23:52:27 +0100

    Update home_page.dart
    Included scrollable feature.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,7 +7,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final fieldText = TextEditingController();
-  String data = '';
+  String text = '';
   String preview = '';
   @override
   Widget build(BuildContext context) {
@@ -32,84 +32,89 @@ class _HomeState extends State<Home> {
                 onPressed: () {
                   fieldText.clear();
                   setState(() {
-                    data = '';
+                    text = '';
                     preview = '';
                   });
                 }),
           ),
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              color: Colors.grey[300],
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 60.0),
-                    child: Center(
-                      child: Container(
-                          width: 200,
-                          height: 150,
-                          /*decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(50.0)),*/
-                          child: Image.asset('assets/brand-logo.png')),
-                    ),
+      body: Container(
+        color: Colors.grey[300],
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  color: Colors.grey[300],
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 60.0),
+                        child: Center(
+                          child: Container(
+                              width: 200,
+                              height: 150,
+                              /*decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(50.0)),*/
+                              child: Image.asset('assets/brand-logo.png')),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        child: Text("Type Your Text",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: TextField(
+                          controller: fieldText,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Your Text',
+                              hintText: 'Type here...'),
+                          onChanged: (value) {
+                            setState(() {
+                              text = value;
+                            });
+                          },
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            preview = text;
+                          });
+                        },
+                        child: Text('Preview',
+                            style: TextStyle(
+                              fontSize: 20,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        width: 300,
+                        child: Text(
+                          preview ?? '',
+                          style: TextStyle(
+                              fontSize: 21, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    child: Text("Type Your Text",
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold)),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    child: TextField(
-                      controller: fieldText,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Your Text',
-                          hintText: 'Type here...'),
-                      onChanged: (value) {
-                        setState(() {
-                          data = value;
-                        });
-                      },
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        preview = data;
-                      });
-                    },
-                    child: Text('Preview',
-                        style: TextStyle(
-                          fontSize: 20,
-                        )),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Container(
-                    width: 300,
-                    child: Text(
-                      preview ?? '',
-                      style:
-                          TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
